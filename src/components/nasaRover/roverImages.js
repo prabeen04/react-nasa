965e3e97b0d7b12fb6f4787e13a8dadfd9f6855a
Prabeen04 <prabeen.strange@gmail.com>
2018-06-17 18:43:25 +0530

    image camera name and date returned from imagerenderer

--- src/components/nasaRover/roverImages.js
+++ src/components/nasaRover/roverImages.js
@@ -3,11 +3,16 @@ import React from 'react'
 export default (props) => {
     let imageRenderer = props.images.map((image) => {
         console.log(image.img_src)
+
+        return <div key={image.id}>
+                    <p>{image.camera.name}: {image.earth_date}</p>
+                    <img key={image.id} src={image.img_src} />
+                </div>
     })
-  return (
-    <div>
-        <h3>Rover Images</h3>
-        {imageRenderer}
-    </div>
-  )
+    return (
+        <div>
+            <h3>Rover Images</h3>
+            {imageRenderer}
+        </div>
+    )
 }
