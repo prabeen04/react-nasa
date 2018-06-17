3d6e7ed0eef5ecf8650523302fe1ab3066dde261
Prabeen04 <prabeen.strange@gmail.com>
2018-06-17 19:07:34 +0530

    styles added

--- src/components/nasaRover/roverImages.js
+++ src/components/nasaRover/roverImages.js
@@ -1,17 +1,15 @@
 import React from 'react'
-
 export default (props) => {
     let imageRenderer = props.images.map((image) => {
         console.log(image.img_src)
 
-        return <div key={image.id}>
+        return <div key={image.id} className="rover-image-wrapper">
                     <p>{image.camera.name}: {image.earth_date}</p>
-                    <img key={image.id} src={image.img_src} />
+                    <img className="rover-image" src={image.img_src} />
                 </div>
     })
     return (
-        <div>
-            <h3>Rover Images</h3>
+        <div className="rover-wrapper">
             {imageRenderer}
         </div>
     )
