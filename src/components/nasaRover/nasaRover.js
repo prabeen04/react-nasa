0e35d86dd4c3681e9de54f84079b6bd0f0ea3502
Prabeen04 <prabeen.strange@gmail.com>
2018-06-17 18:22:04 +0530

    state and setstate added

--- src/components/nasaRover/nasaRover.js
+++ src/components/nasaRover/nasaRover.js
@@ -2,29 +2,40 @@ import React, { Component } from 'react';
 import { NASA_API_KEY } from '../../config/index';
 import axios from 'axios';
 
-const apiURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${NASA_API_KEY}`;
+const apiURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=${NASA_API_KEY}`;
 
 class NasaRover extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-
-        this.state = {}
+        this.state = {
+            images: [],
+            fetching: false,
+            error: null
+        }
     }
-    componentDidMount(){
+    componentDidMount() {
         axios.get(apiURL)
-        .then(res => {
-            console.log(res.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    images: res.data,
+                    fetching: false
+                })
+            })
+            .catch(err => {
+                console.log(err)
+                this.setState({
+                    fetching: false,
+                    error: err
+                })
+            })
     }
-  render() {
-    return (
-      <div>
-        <h1>NasaRover Component</h1>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h1>NasaRover Component</h1>
+            </div>
+        )
+    }
 }
 export default NasaRover;
